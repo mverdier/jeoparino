02e9e67fca3f6fbe273152035ee25916f0138d19
Martin <mart.verdier@gmail.com>
2018-04-21 23:40:04 +0200

    Initialized Node and first test codebase

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+    
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('jebaited', function(msg){
+  	io.emit('jebaited', msg);
+    console.log('jebaited used by ' + msg);
+  });
+
+  socket.on('buzzer', function(msg){
+  	io.emit('buzzer', msg);
+    console.log(msg[0] + ' buzzed at ' + msg[1]);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
